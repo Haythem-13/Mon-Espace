83349eb5e8635f1464b8f4d70257c43b4d0543a9
unknown <haythem.nasfi@gmail.com>
2024-02-19 05:39:01 +0100

    Mon-Espace

--- server/server.js
+++ server/server.js
@@ -1,9 +1,24 @@
-const express = require('express')
-const port = 5000;
+const express = require('express');
+require('dotenv').config();
+const connection = require('./connection');
+const auth = require('./middelware/auth');
 
-const app=express();
+connection();
+const app = express();
+const port = 5000;
+app.use(express.json());
+
+const cors = require('cors');
+app.use(cors());
+
+const accountRoute = require('./Routers/accounts');
+
+const { createNewAccounts } = require('./Controllers/accounts');
+const { getAllUsersHistory } = require('./Controllers/bankHistory');
+app.post('/accounts/create', createNewAccounts);
+app.get('/bankhistory', getAllUsersHistory); 
 
 app.listen(port, () => {
-    console.log(`Server startlistening on port ${port}`);
+    console.log(`Server start listening on port ${port}`);
   });
   
